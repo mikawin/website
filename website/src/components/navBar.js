9d914dce1c8b11ceeb3226a72b859e7d2e270617
Mikayla Win <mikayla.win@comcast.net>
2024-06-28 18:11:23 -0700

    implemented mobile view; the home page is now also responsive and changes based on device size

--- website/src/components/navBar.js
+++ website/src/components/navBar.js
@@ -1,21 +1,42 @@
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import 'bulma/css/bulma.min.css';
-import './navBar.css'
-
+import './navBar.css';
 
 function NavBar() {
     const navigate = useNavigate();
-    console.log(navigate);
+    const [isActive, setIsActive] = useState(false);
+
+    const toggleBurgerMenu = () => {
+        setIsActive(!isActive);
+    };
 
     return (
         <nav className="navbar is-white" role="navigation" aria-label="main navigation">
-            <div className="navbar-start">
-                <div className="navbar-item subtitle is-5"></div>
-                <a className="navbar-item subtitle is-5" onClick={() => navigate('/')}>Home</a>
-                <a className="navbar-item subtitle is-5" onClick={() => navigate('/experience')}>Experience</a>
-                <a className="navbar-item subtitle is-5" onClick={() => navigate('/projects')}>Projects</a>
-                <a className="navbar-item subtitle is-5" href='https://github.com/mikawin'>Github</a>
-                <div className="navbar-item subtitle is-5"></div>
+            <div className="navbar-brand">
+                <a
+                    role="button"
+                    className={`navbar-burger burger ${isActive ? 'is-active' : ''}`}
+                    aria-label="menu"
+                    aria-expanded="false"
+                    onClick={toggleBurgerMenu}
+                >
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div className={`navbar-menu ${isActive ? 'is-active' : ''}`}>
+                <div className="navbar-start">
+                    <a className="navbar-item subtitle is-5"></a>
+                    <a className="navbar-item subtitle is-5" onClick={() => navigate('/website')}>Home</a>
+                    <a className="navbar-item subtitle is-5" onClick={() => navigate('/wesbite/experience')}>Experience</a>
+                    <a className="navbar-item subtitle is-5" onClick={() => navigate('/website/projects')}>Projects</a>
+                    <a className="navbar-item subtitle is-5" href='https://github.com/mikawin'>Github</a>
+                    <a className="navbar-item subtitle is-5"></a>
+
+                </div>
             </div>
         </nav>
     );
--- website/src/pages/Home.js
+++ website/src/pages/Home.js
@@ -1,63 +1,73 @@
 import React from 'react';
-import { BrowserView, MobileView } from 'react-device-detect';
-
-
+import { isMobile } from 'react-device-detect';
 import './Home.css';
 import 'bulma/css/bulma.min.css';
-import NavBar from '../components/navBar'
+import NavBar from '../components/navBar';
 import me_image from './me.jpg';
 import grad_image from './grad_pic.jpg';
 
 function Home() {
-
     return (
         <div>
-            <BrowserView>
-                <NavBar />
-                <div className='container'>
-                    <div className="columns">
-                        <div className='column is-two-thirds' >
-                            <div className='intro'>
-                                Hello ! My name is Mikayla Win
-                            </div>
-                            <div className='subtitle-container about-me-font'>
-                                I recently graduated from the University of California, Santa Cruz, where
-                                I majored in both Business Management Economics and Computer Science.  During my time
-                                at UCSC, I was a part of the school's Women's Soccer Team and Alpha Kappa Psi - Chi Gamma Chapter.
-                                From the courses I took to the memories I made, I learned so much about myself. I grew
-                                fond of the power of data and the stories it tells when you dig deeper. After
-                                realizing my excitement, I am currently working to find a job as an analyst.
-
-                            </div>
-                        </div>
-                        <div className='column'>
-
-                        </div>
-                        <div className='column'>
-                            <div className='subtitle is-3   '>Contact Me</div>
-                            <div className='subtitle is-5'>Email</div>
-                            <div className='su  btitle is-5'>Phone Number</div>
-
-                        </div>
+            <NavBar />
+            {isMobile ? (
+                <div className="mobile-view">
+                    <div className='mobile-contact-me-container'>
+                        <div className='mobile-contact-me-font'>Contact Me</div>
+                        <div className='mobile-info-font'>Email</div>
+                        <div className='mobile-info-font'>Phone Number</div>
+                    </div>
+                    <div className='mobile-intro'>
+                        Hello! My name is Mikayla Win
+                    </div>
+                    <div className='mobile-subtitle-container mobile-about-me-font'>
+                        I recently graduated from the University of California, Santa Cruz, where
+                        I majored in both Business Management Economics and Computer Science. During my time
+                        at UCSC, I was a part of the school's Women's Soccer Team and Alpha Kappa Psi - Chi Gamma Chapter.
+                        From the courses I took to the memories I made, I learned so much about myself. I grew
+                        fond of the power of data and the stories it tells when you dig deeper. After
+                        realizing my excitement, I am currently working to find a job as an analyst.
                     </div>
 
+                    <div className='mobile-circular-image  bottom'>
+                        <img src={me_image} alt="me" width="100%" />
+                    </div>
+                    <div className='mobile-grad-image'>
+                        <img src={grad_image} alt="me" width="100%" />
+                    </div>
+                </div>
+            ) : (
+                <div className="columns">
+                    <div className='column is-two-thirds'>
+                        <div className='intro'>
+                            Hello! My name is Mikayla Win
+                        </div>
+                        <div className='subtitle-container about-me-font'>
+                            I recently graduated from the University of California, Santa Cruz, where
+                            I majored in both Business Management Economics and Computer Science. During my time
+                            at UCSC, I was a part of the school's Women's Soccer Team and Alpha Kappa Psi - Chi Gamma Chapter.
+                            From the courses I took to the memories I made, I learned so much about myself. I grew
+                            fond of the power of data and the stories it tells when you dig deeper. After
+                            realizing my excitement, I am currently working to find a job as an analyst.
+                        </div>
+                    </div>
+                    <div className='column'>
+                        {/* You can add content to this column if needed */}
+                    </div>
+                    <div className='column'>
+                        <div className='contact-me-font'>Contact Me</div>
+                        <div className='info-font'>Email</div>
+                        <div className='info-font'>Phone Number</div>
+                    </div>
                     <div className='circular-image bottom'>
-                        <img src={me_image} alt="me" width="350px" />
+                        <img src={me_image} alt="me" width="100%" />
                     </div>
                     <div className='grad-image'>
-                        <img src={grad_image} alt="me" width="350px" />
+                        <img src={grad_image} alt="me" width="100%" />
                     </div>
                 </div>
-
-            </BrowserView >
-
-            <MobileView>
-                <div className='intro'>
-                    Hello ! My name is Mikayla Win
-                </div>
-            </MobileView>
+            )}
         </div>
-
     );
 }
 
